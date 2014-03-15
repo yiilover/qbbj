b8517ee6e19638d708caa752332a0cc1a32fd3cc
yiilover <yiilover@gmail.com>
2014-03-16 01:02:40 +0800

    sql

--- hisql.sql
+++ hisql.sql
@@ -1,1 +1,9 @@
 delete from qb_sort where fid<41;
+alter table qb_guestbook_content add companyname varchar(255) NOT NULL default '';
+alter table qb_guestbook_content add truename varchar(255) NOT NULL default '';
+alter table qb_guestbook_content add phone varchar(255) NOT NULL default '';
+alter table qb_guestbook_content add deadline varchar(255) NOT NULL default '';
+alter table qb_guestbook_content add attach1 varchar(255) NOT NULL default '';
+alter table qb_guestbook_content add attach2 varchar(255) NOT NULL default '';
+alter table qb_guestbook_content add attach3 varchar(255) NOT NULL default '';
+alter table qb_guestbook_content add attachurl varchar(255) NOT NULL default '';
